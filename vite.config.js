b5984506962a1098f4c272294452927624329ec5
johnm1ck <6731351221@student.chula.ac.th>
2026-01-04 11:26:18 +0700

    Update config for 7WaDud repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Makes paths relative so index.html works better if opened directly
+  base: '/7WaDud/', // Set to your repository name
   server: {
     host: true, // Listen on all addresses
     port: 8080,
